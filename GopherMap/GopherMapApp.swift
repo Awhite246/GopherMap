5242f29ac668ae03f3d357ad840995a9b2a8359b
Awhite246 <90467271+Awhite246@users.noreply.github.com>
2023-11-03 23:35:53 -0500

    Initial Commit

---
+++ GopherMap/GopherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GopherMapApp.swift
+//  GopherMap
+//
+//  Created by Alistair White on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct GopherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
